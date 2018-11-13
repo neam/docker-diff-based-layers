203d2d4c9a116d587decc4aa75c2cc036a163c98
danielsufrin <dsufrin@greenlots.com>
2018-11-13 12:44:05 -0500

    making sure hard links match beginning of line, improved comments

--- create-changelog-to-go-from-old-to-new.sh
+++ create-changelog-to-go-from-old-to-new.sh
@@ -5,7 +5,9 @@ echo "List of changes necessary to go from old image contents to new: "
 rsync -a -x --human-readable --delete-after --checksum --dry-run --itemize-changes --exclude .docker-image-diff "$RESTRICT_DIFF_TO_PATH/" "rsync://rsync@old/root$RESTRICT_DIFF_TO_PATH/" | tee $OUT/changes.rsync.log
 
 # Add files to add to a tar archive
-cat $OUT/changes.rsync.log | grep  -E '^<f|^cL|hf' | while read -a cols; do echo "$RESTRICT_DIFF_TO_PATH/"${cols[@]:1}; done > $OUT/files-to-add_2.list
+cat $OUT/changes.rsync.log | grep  -E '^<f|^cL|^hf' | while read -a cols; do echo "$RESTRICT_DIFF_TO_PATH/"${cols[@]:1}; done > $OUT/files-to-add_2.list
+
+# soft links (^cL in the grep above) have -> in the list which must be removed.  Hard links ^hf have =>
 cat $OUT/files-to-add_2.list | grep -oP '^\K.*?(?= ->| =>|$)' > $OUT/files-to-add.list
 tar -cf $OUT/files-to-add.tar -T $OUT/files-to-add.list 2>&1 -C /
 
@@ -21,6 +23,6 @@ echo "Number of files to remove: "
 wc $OUT/files-to-remove.list
 echo
 echo "Changes not accounted for: "
-cat $OUT/changes.rsync.log | grep -v -E '^<f|^cL|hf'  | grep -v '^*deleting'
+cat $OUT/changes.rsync.log | grep -v -E '^<f|^cL|^hf'  | grep -v '^*deleting'
 echo
 echo "Press CTRL-C to continue"
